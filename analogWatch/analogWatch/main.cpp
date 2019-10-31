2a1219669062284bc1cf3f7ec280dda104a2811b
Babdodook <48229283+Babdodook@users.noreply.github.com>
2019-10-31 20:16:24 +0900

    원까지그림
    ㅇㅇ

--- analogWatch/analogWatch/main.cpp
+++ analogWatch/analogWatch/main.cpp
@@ -6,7 +6,7 @@
 #include<Windows.h>
 using namespace std;
 
-#define SIZE 21
+#define SIZE 30
 #define DEG2RAD M_PI/180
 
 int Circle[SIZE][SIZE];
@@ -33,41 +33,41 @@ void setPixel(int x, int y)
 
 void drawCircle()
 {
-	int r = 10;
+	int r = 13;
 	int x, y;
 
 
-	for (int i = 1; i <= 12; i++)
+	for (int i = 1; i < 360; i++)
 	{
-		x = cos(360 / i * DEG2RAD) * r + 10;
-		y = sin(360 / i * DEG2RAD) * r + 10;
-
-		cout << x << ", " << y << endl;
-		Circle[y][x] = 2;
-
+		if (i != 90)
+		{
+			x = cos(i * DEG2RAD) * r + 15;
+			y = sin(i * DEG2RAD) * r + 15;
+			Circle[y][x] = 1;
+		}
+		// cout << x << ", " << y << endl;
 	}
 
-	Circle[SIZE / 2][SIZE / 2] = 2;
+	Circle[SIZE / 2][SIZE / 2] = 1;
+	Circle[SIZE / 2 - 1][SIZE / 2] = 1;
+	Circle[SIZE / 2][SIZE / 2 - 1] = 1;
+	Circle[SIZE / 2 - 1][SIZE / 2 - 1] = 1;
 
 	for (int i = 0; i < SIZE; i++)
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
-			if (Circle[i][j] == 0)
-			{
-				cout << "бс";
-			}
-			else if (Circle[i][j] == 2)
+			if (Circle[i][j] == 1)
 			{
 				cout << "бр";
 			}
-			else
-				cout << "бб";
+			else if (Circle[i][j] == 0)
+			{
+				cout << "бс";
+			}
 		}
 		cout << endl;
 	}
-
-	cout << x << ", " << y << endl;
 
 }
 
